695044f5f7036f874f8ce7139ac9f35d270fc12f
Jacopo Autorino <j.autorino@contrader.it>
2023-05-16 22:32:50 +0200

    mergiato base di partenza

---
+++ spring-rest/src/main/java/it/contrader/service/DettagliCloneService.java
@@ -0,0 +1,10 @@
+package it.contrader.service;
+
+import it.contrader.dto.DettagliCloneDTO;
+import it.contrader.model.DettagliClone;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DettagliCloneService extends AbstractService<DettagliClone, DettagliCloneDTO> {
+
+}
