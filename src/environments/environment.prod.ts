7f54058adef58bc621e63720811dcdea161d06e7
diegoFCJ <diegoeffe96@gmail.com>
2023-09-20 10:37:10 +0200

    back route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  APIEndpoint: "http://https://front-diego76io-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com//"
+  APIEndpoint: "http://https://hackathon-team-targaryen-git-diego76io-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com//"
 };
