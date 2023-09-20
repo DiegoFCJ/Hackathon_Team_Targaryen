7bcc7809239f1bdf45c3af51106bc453e3406047
diegoFCJ <diegoeffe96@gmail.com>
2023-09-20 10:31:34 +0200

    environment.prod change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  APIEndpoint: "http://host:ip/"
+  APIEndpoint: "http://https://front-diego76io-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com//"
 };
