4a97d0c4af1fba9be29a4a139a355fdb8640a5da
DiegoFCJ <103056792+DiegoFCJ@users.noreply.github.com>
2023-10-06 20:33:49 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  APIEndpoint: "https://172.30.53.228:8080/"
+  APIEndpoint: "http://hackat-back-diego76io-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/"
 };
